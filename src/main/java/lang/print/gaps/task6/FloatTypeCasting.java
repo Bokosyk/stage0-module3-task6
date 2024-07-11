af2ee8cf642e957ba05a076ca0ed5597b8697a39
Leandro Parrado <lparrdev@gmail.com>
2024-07-11 19:35:47 -0400

    TS1

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,20 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        int intNumber = (int) numberToBeRounded;
+        float decimalRemainder = Math.abs(intNumber - numberToBeRounded);
+
+        if (decimalRemainder >= 0.5){
+            System.out.println(intNumber + 1);
+        }
+        else {
+            System.out.println(intNumber);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        FloatTypeCasting roundnumber = new FloatTypeCasting();
+        roundnumber.roundNumber((float)19.5);
     }
 }
